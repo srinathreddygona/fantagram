63dc740fce1e8345f839a9c7d5f883d0d7d628a1
srinathreddygona <gsrinathreddy12@gmail.com>
2025-02-15 00:14:16 +0530

    inital commit

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,23 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+};
+
+export const client = new Client();
+
+client.setEndpoint(appwriteConfig.url);
+console.log(import.meta.env.VITE_APPWRITE_URL);  // Check if the URL is correct
+console.log("i am atconfig.ts")
+client.setProject(appwriteConfig.projectId);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
